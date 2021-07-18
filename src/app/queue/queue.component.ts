a4db06ce9bb6c193ae49c94db76e99b0bfe54417
tom <marczakx@gmail.com>
2021-07-18 22:35:17 +0200

    Update queue component - highlight

--- src/app/queue/queue.component.ts
+++ src/app/queue/queue.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { QueueItem } from '../queue-item';
 import { Subscription, Observable, timer } from 'rxjs';
 import { QueueService } from '../queue.service';
+import { SharedService} from '../shared.service';
 
 @Component({
   selector: 'app-queue',
@@ -14,13 +15,17 @@ export class QueueComponent implements OnInit {
 
   everySecond: Observable<number> = timer(0, 1000);
 
-  constructor(private queueService: QueueService) { }
-
+  id: string;
+  pseudonym:string;
+  constructor(private queueService: QueueService, private sharedService:
+    SharedService) { }
   ngOnInit(): void {
     this.subscription = this.everySecond.subscribe((seconds) =>{
-        this.queueService.findAll().subscribe(data => {
-            this.queue = data;
-        });
+      this.id = this.sharedService.getId();
+      this.pseudonym = this.sharedService.getPseudonym();
+      this.queueService.findAll().subscribe(data => {
+        this.queue = data;
+      });
     });
   }
   ngOnDestroy(): void {
