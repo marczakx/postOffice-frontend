191db03501fc3cfdee6d96646f82d3dd8f182630
tom <marczakx@gmail.com>
2021-07-19 10:11:19 +0200

    Split find-by-pseudonym-form component

--- src/app/find-by-pseudonym-form/find-by-pseudonym-form.component.ts
+++ src/app/find-by-pseudonym-form/find-by-pseudonym-form.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { RequestService } from '../request.service';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { Subscription, Observable, timer } from 'rxjs';
-import { QueueService } from '../queue.service';
+import { SharedService } from './../shared.service';
+
 @Component({
   selector: 'app-find-by-pseudonym-form',
   templateUrl: './find-by-pseudonym-form.component.html',
@@ -11,29 +10,14 @@ import { QueueService } from '../queue.service';
 export class FindByPseudonymFormComponent implements OnInit {
 
   pseudonym = new FormControl('');
-  resultNumber = new FormControl('');
-  resultTime = new FormControl('');
-  private subscription: Subscription;
-  everySecond: Observable<number> = timer(0, 1000);
   requestForm: FormGroup = this.builder.group({
-  pseudonym: this.pseudonym
+    pseudonym: this.pseudonym
   });
-  constructor(private queueService: QueueService, private builder: FormBuilder) {
+  constructor(private builder: FormBuilder, private sharedService:SharedService) {
   }
-  onSubmit(){  this.subscription = this.everySecond.subscribe((seconds) =>{
-    this.queueService.getTimeByPseudonym(this.pseudonym.value).subscribe(data => {
-            this.resultTime.setValue("Czas oczekiwania " +data);
-        });
-  
-    this.queueService.getNumberByPseudonym(this.pseudonym.value).subscribe(data => {
-            this.resultNumber.setValue("Liczba osób w kolejce przed użytkownikiem " +data);
-        });
-     });
+  onSubmit(){
+    this.sharedService.setPseudonym(this.pseudonym.value);
   }
   ngOnInit(): void {
   }
-
-
 }
-
-
